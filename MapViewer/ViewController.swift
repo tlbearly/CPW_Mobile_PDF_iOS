2794cb298278f1bb090f68cc111d08a121283986
Tammy Bearly <tbearly@colostate.edu>
2020-04-10 09:08:06 -0600

    Initial Commit

---
+++ MapViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapViewer
+//
+//  Created by Brittney Bearly on 4/10/20.
+//  Copyright © 2020 Colorado Parks and Wildlife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
