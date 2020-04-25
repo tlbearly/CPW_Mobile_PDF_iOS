f2027e14a3e1c90212a25e444b3fecc8f9b91f6e
Tammy Bearly <tbearly@colostate.edu>
2020-04-24 18:07:07 -0600

    4-24-20 file picker

--- MapVieweriOS/Controllers/AddMapsViewController.swift
+++ MapVieweriOS/Controllers/AddMapsViewController.swift
@@ -5,8 +5,12 @@
 //  Created by Tammy Bearly on 4/23/20.
 //  Copyright © 2020 Colorado Parks and Wildlife. All rights reserved.
 //
+// File Picker help https://www.google.com/search?client=firefox-b-1-d&q=swift+file+picker#kpvalbx=_2GqjXuXrEJrL0PEPi-id2AI26
+//
+
 
 import UIKit
+import MobileCoreServices
 
 class AddMapsViewController: UIViewController {
 
@@ -14,6 +18,8 @@ class AddMapsViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = "Add Map"
+        // For debugging write a test file to the documents dir
+        //writeDebugPDF(self)
     }
     
 
@@ -26,5 +32,46 @@ class AddMapsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func writeDebugPDF(_ sender: Any){
+        // use in simulater to write Wellington.pdf to the Simulator documents directory.
+        guard let pdfFileURL = Bundle.main.url(forResource: "Wellington", withExtension: "pdf") else {
+            print ("Can't write file: PDF file not found.")
+            return
+        }
+        // destination directory name
+        guard let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Can't get documents directory.")
+            return
+        }
 
+        let fileName = "Wellington.pdf"
+        let destURL = documentsURL.appendingPathComponent(fileName)
+        do {
+            try FileManager.default.copyItem(at:pdfFileURL, to: destURL)
+        }
+        catch{
+            print("can't copy file")
+        }
+    }
+    
+    
+    @IBAction func filePickerClicked(_ sender: PrimaryUIButton) {
+        print("open file picker")
+        //com.adobe.pdf (kUTTypePDF)
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [kUTTypePDF as String], in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    
+}
+
+extension AddMapsViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+       // called when user selects a pdf to import
+        print("selected a pdf. File is in urls[0]")
+        
+    }
 }
